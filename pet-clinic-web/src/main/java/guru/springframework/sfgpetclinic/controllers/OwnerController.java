cdfb061aaf5b11f1abeca7c21a6799ffb4fde004
NsovoBabane-Risima <nsovo.babane@gmail.com>
2022-03-15 04:38:32 +0200

    Create Speciality Map Service Impl. Closes #30

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("owners")
+@RequestMapping("/owners/find")
 public class OwnerController {
 
     private final OwnerService ownerService;
