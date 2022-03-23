8cdd899d737abb38bb203aebcfd222b935d430a1
NsovoBabane-Risima <nsovo.babane@gmail.com>
2022-03-23 09:00:48 +0200

    Create Spring Data JPA Vet Speciality Service #43

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/VetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/VetMapService.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.service.map;
 
+import guru.springframework.sfgpetclinic.model.Speciality;
 import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.service.SpecialityService;
 import guru.springframework.sfgpetclinic.service.VetService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,13 @@ import java.util.concurrent.atomic.AtomicReference;
 @Service
 @Profile({"default", "map"})
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService{
+
+    SpecialityService specialityService;
+
+    public VetMapService(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
     @Override
     public Vet findById(Long id) {
         return super.findById(id);
@@ -39,6 +48,16 @@ public class VetMapService extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet vet) {
+        if(vet.getSpecialities().size() > 0){
+            vet.getSpecialities().forEach(speciality -> {
+                if(speciality.getId() == null){
+                    Speciality savedSpeciality = specialityService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+                }
+            });
+
+        }
+
         return super.save(vet);
     }
 
