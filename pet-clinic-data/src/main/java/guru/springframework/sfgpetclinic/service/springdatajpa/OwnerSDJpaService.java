a3de50cfce9d8f13fd7b924932185cba41cf8d3d
NsovoBabane-Risima <nsovo.babane@gmail.com>
2022-03-23 07:48:50 +0200

    Create Spring Data JPA Service for Owners. Closes #39

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,76 @@
+package guru.springframework.sfgpetclinic.service.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import guru.springframework.sfgpetclinic.repositories.PetRepository;
+import guru.springframework.sfgpetclinic.repositories.PetTypeRepository;
+import guru.springframework.sfgpetclinic.service.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final PetTypeRepository petTypeRepository;
+    private final PetRepository petRepository;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository, PetTypeRepository petTypeRepository, PetRepository petRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petTypeRepository = petTypeRepository;
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owner -> owners.add(owner));
+        return owners;
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        if (!owner.getPets().isEmpty()){
+            for (Pet pet:  owner.getPets()) {
+                if (petRepository.findById(pet.getId()).isEmpty()) {
+                    petRepository.save(pet);
+                }
+                if (petTypeRepository.findById(pet.getPetType().getId()).isEmpty()){
+                    petTypeRepository.save(pet.getPetType());
+                }
+            }
+        }
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        ownerRepository.delete(owner);
+    }
+
+    @Override
+    public void deleteAll(Map<Long, Owner> map) {
+        List<Owner> owners = new ArrayList<>();
+        map.values().forEach(owner -> owners.add(owner));
+        ownerRepository.deleteAll(owners);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+}
